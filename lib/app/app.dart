582e8ab7f46d15d40f26dbf8ef14ee04dfd2faa7
Ahmed <medicationreminder88@gmail.com>
2024-12-13 23:38:55 +0200

    integrate app with firebase

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../screens/splash_screen.dart';
+import '../views/screens/splash_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: LoginScreen(),
     );
   }
 }
