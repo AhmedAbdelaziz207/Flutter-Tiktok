35ee44e865838964e773dbd219b3aac2cd0f347e
Ahmed <medicationreminder88@gmail.com>
2024-12-14 08:23:00 +0200

    Integrate app with supabase create supabase service file

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+// app colors
+Color primaryColor = const Color(0xFFd53856);
+
+// supabase constants
+const supabaseUrl = 'https://tiktokclone.supabase.co';
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqc2ZxdGNubmx2cml4dWJ0bG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQxNDE0MDYsImV4cCI6MjA0OTcxNzQwNn0.KRtZgNM0Lo0HmjI-4oSSpBr4d5iennvj7LY6_XEXC6k";
